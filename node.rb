d18e6679cdd8a70bb7fe413357240595734c8803
JK47 <jhnkotz@gmail.com>
2015-07-21 11:20:09 -0500

    adding some bst logic

--- node.rb
+++ node.rb
@@ -1,5 +1,5 @@
 class Node
-  def initialize(value, child, parent)
+  def initialize(value, child=nil, parent=nil)
     @value = value
     @parent = parent
     @child = child
@@ -17,13 +17,26 @@ class Node
     @value
   end
 
+  def add_node(value, tree)
+    if value > tree.get_value && (tree.get_child.get)
+  end
+
   def build_tree(array)
     if array.length == 0
       return nil
     end
-    array.each_with_index{ |value, i|
 
+    tree = new Node(array[0])
+    array[1...-1].each{ |val|
+      tree.add_node(val, tree)
     }
+
+
+
+
+  end
+
+
 
 
 end
